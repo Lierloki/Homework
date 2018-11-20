87553849422290235579870eaeb00d4b43779b67
LierLoki <gotsiridze.saba@gmail.com>
2018-11-20 21:02:39 +0300

    homework

---
+++ Assets/Scripts/WallCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCreator : MonoBehaviour {
+
+    public GameObject wall1;
+    public float harder = 0.001f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (float b = -6.5f; b < 6.6; b= b+2)
+        {
+            if (Random.value < harder)
+            {
+                Instantiate(wall1.transform, new Vector3(b, 1, 80), Quaternion.identity);
+                harder = harder + 0.0001f;
+            }
+        }
+    }
+}
---
+++ Assets/unlimited.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class unlimited : MonoBehaviour {
+
+    public float forwardForce = -2000f;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+		
+	}
+}
